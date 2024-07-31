2244739d31a7b33b69875b1ffee62bbc8cc8baf1
syamsaiyek <msyamsundarsai@gmail.com>
2024-07-31 22:15:56 +0530

    some

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import DataPage from './components/DataPage'
-import Table from './components/Table'
+
 import Table2 from './components/Table2'
 
 
 const App = () => {
   return (
     <div>
-      {/* <DataPage></DataPage> */}
-      {/* <Table></Table> */}
+     
       <Table2></Table2>
       
     </div>
